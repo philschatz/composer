9857c0a1f64599822bf1b21eea719f49ba4e931a
Michael Aufreiter <ma@zive.at>
2012-05-08 19:41:06 -0400

    Document storage API.

--- server.js
+++ server.js
@@ -6,14 +6,14 @@ var Data = require('./lib/data');
 var _ = require('underscore');
 var util = require('./src/server/util.js')
 
+var DocumentStorage = require('./src/server/document_storage.js');
+var ds = new DocumentStorage();
 
 // App config
-// -----------
+// ===========
 
 global.config = JSON.parse(fs.readFileSync(__dirname+ '/config.json', 'utf-8'));
 global.example = fs.readFileSync(__dirname+ '/data/example.json', 'utf-8');
-
-
 
 // Express.js Configuration
 // -----------
@@ -44,6 +44,10 @@ function serveStartpage(req, res) {
 }
 
 // Web server
+// ===========
+
+
+// Style sheets
 // -----------
 
 app.get('/styles.css', function(req, res) {
@@ -54,7 +58,32 @@ app.get('/styles.css', function(req, res) {
   });
 });
 
+// Read document from store
+// -----------
+
+app.get('/read/:id', function(req, res) {
+  ds.read(req.params.id, function(err, data) {
+    res.send(data);
+  });
+});
+
+// Store a document
+// -----------
+
+app.put('/write', function(req, res) {
+  var doc = req.body;
+  ds.write(doc, function(err, rev) {
+    res.send('Document successfully stored. New revision: '+rev);
+  });
+});
+
+// Serve startpage
+// -----------
+
 app.get('/', serveStartpage);
+
+// Start server
+// -----------
 
 app.listen(config['server_port'], config['server_host'], function (err) {
   console.log('READY: Substance is listening http://'+config['server_host']+':'+config['server_port']);
---
+++ src/server/document_storage.js
@@ -0,0 +1,25 @@
+var child_process = require('child_process'),
+    fs = require('fs'),
+    db = require('riak-js').getClient(),
+    _ = require('underscore');
+    
+
+var DocumentStorage = function() {
+
+};
+
+_.extend(DocumentStorage.prototype, {
+  write: function(document, cb) {
+    db.save('documents', document.id, document, function(err) {
+      cb(err);
+    });
+  },
+
+  read: function(id, cb) {
+    db.get('documents', id, function(err, doc) {
+      cb(err, doc);
+    });
+  }
+});
+
+module.exports = DocumentStorage;
