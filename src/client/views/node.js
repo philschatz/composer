6afe607097e6edaa29286aedf7fbf2c91830aaca
phil <phil@cnx.org>
2012-06-20 13:21:54 -0400

    Drag and drop are no longer off-by-one
    insertBefore vs insertAfter. The local view was different than the
    remote view

--- src/client/views/node.js
+++ src/client/views/node.js
@@ -82,7 +82,7 @@ sc.views.Node = Dance.Performer.extend(_.extend({}, s.StateMachine, {
         // Get back the node id from the html id (replace "_" with "/")
         var src = _.htmlId(ui.draggable.attr('id')).replace(/_/g, '/');
         that.document.execute({command:"node:move", params: {user: $('.username').val(), nodes: [src], target: that.model._id}});
-        ui.draggable.insertBefore($(this));
+        ui.draggable.insertAfter($(this));
         ui.draggable.attr('style', '');
       }
     });
