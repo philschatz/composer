5685fcce08d7a06137fe86a42a4df03a4f580474
phil <phil@cnx.org>
2012-06-21 19:47:47 -0400

    Crude parsing of HTML elements
    Just to test how easy it is to convert DOM elements into a usable model

--- src/client/boot.js
+++ src/client/boot.js
@@ -1,8 +1,67 @@
 $(function() {
 
-  sc.models.Document.load("example.json", function(err, doc) {
+
+  var doc = {
+    id: "substance-composer",
+    created_at: "2012-04-10T15:17:28.946Z",
+    updated_at: "2012-04-10T15:17:28.946Z",
+    nodes: {},  //populated later
+    head: null, //populated later
+    tail: null, //populated later
+    rev: 3
+  };
+  
+  var nextId = 0;
+  function htmlParser($els) {
+    var nodeIds = [];
+    var prevNode = null;
+    var prevId = null;
+    _.each($els, function(el) {
+      var $el = $(el);
+      var tag = $el.get(0).tagName.toLowerCase();
+      var id = $el.attr('id') || "autoid-" + tag + "/" + nextId++;
+      var node = { type: [ '/type/node' ] };
+      if(tag == 'p') {
+        node.type.push('/type/text');
+        node.content = el.innerHTML;
+      } else if(tag == 'div' && $el.hasClass('section')) {
+        node.type.push('/type/section');
+        $title = $el.find("> h2.title");
+        node.name = $title ? $title.get(0).innerHTML : '[Insert Title]';
+        
+        node.children = htmlParser($el.find("> *:not(h2.title)"));
+        
+      } else {
+        console.log("skipping element with tag '" + tag + "'");
+        return;
+      }
+      node.prev = prevId;
+      node.next = null;
+      
+      nodeIds.push(id);
+      
+      // Link up the previous node's next to this node
+      if (prevNode) prevNode.next = id;
+      
+      doc.nodes[id] = node;
+      if (!(doc.head)) doc.head = id;
+      doc.tail = id;
+      
+      // Get ready for the next node
+      prevId = id;
+      prevNode = node;
+    });
+    
+    return nodeIds;
+  }
+
+
+  htmlParser($('#container > *'));
+  console.log("Parsed HTML into ", doc);
+  
+  //sc.models.Document.load("example.json", function(err, doc) {
     window.composer = new Substance.Composer({model: doc, el: '#container', user: "michael"});
     composer.start();
-  });
+  //});
 
 });
